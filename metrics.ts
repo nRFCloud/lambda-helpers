25e3be4b4c011c8ced66d2d1fa12d3a1b6772cca
Markus Tacker <Markus.Tacker@NordicSemi.no>
2026-04-29 09:52:56 +0200

    fix: move the log statement into the function
    This way it is not printed whenever code
    is imported from this module, only when
    the metricsForComponent() function is used.

--- metrics.ts
+++ metrics.ts
@@ -3,9 +3,6 @@ import { Metrics } from '@aws-lambda-powertools/metrics'
 const registry: Record<string, Metrics> = {}
 export type AddMetricsFn = (...args: Parameters<Metrics['addMetric']>) => void
 
-const metricsEnabled = process.env.DISABLE_METRICS !== '1'
-console.debug(`[Metrics]`, metricsEnabled ? `Enabled` : `Disabled`)
-
 /**
  * Manages the instantiation of a Metrics object (unless Metrics are disabled)
  */
@@ -16,7 +13,9 @@ export const metricsForComponent = (
 	metrics: Metrics
 	track: AddMetricsFn
 } => {
+	const metricsEnabled = process.env.DISABLE_METRICS !== '1'
 	if (registry[component] === undefined) {
+		console.debug(`[Metrics]`, metricsEnabled ? `Enabled` : `Disabled`)
 		registry[component] = new Metrics({
 			namespace,
 			serviceName: component,
